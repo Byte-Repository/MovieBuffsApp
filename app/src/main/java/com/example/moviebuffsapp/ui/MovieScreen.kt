c4ccb9405f584bb8765870eb15f186e99f247e9b
Cesar Ortega <cortegafrias1@buffs.wtamu.edu>
2023-12-07 17:27:44 -0600

    Commit.19

--- app/src/main/java/com/example/moviebuffsapp/ui/MovieScreen.kt
+++ app/src/main/java/com/example/moviebuffsapp/ui/MovieScreen.kt
@@ -56,6 +56,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -105,7 +106,9 @@ fun HomeScreen(
                                 viewModel.updateCurrentMovie(it)
                                 viewModel.navigateToDetailPage()
                             },
-                            contentPadding = contentPadding
+                            contentPadding = contentPadding,
+                            modifier = Modifier
+                                .fillMaxWidth()
                         )
                     }
                     else -> {
